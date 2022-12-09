34ebc01512fbddc7f842dd409970be6374cc3c15
yashnerkar <yashnerkar512@gmail.com>
2022-12-10 03:20:11 +0530

    updated styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Canvas from "./components/Canvas"
 import CircleIcon from '@mui/icons-material/Circle';
 import SquareIcon from '@mui/icons-material/Square';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Stack from '@mui/material/Stack';
+
 function App() {
   const [objects, setObjects] = useState({
     circle: false,
@@ -30,37 +34,57 @@ function App() {
       }
     })
   }
-  console.log(shapes);
 
+  useEffect(() => {
+    localStorage.setItem('shapes', JSON.stringify(shapes));
+  }, [shapes])
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem('shapes'));
+    setShapes(items);
+  }, [])
 
+  const handleClearCanvas = () => {
+    setShapes({
+      circle: [],
+      square: []
+    })
+  }
 
   function handleDragEnd(e) {
     const name = e.target.attrs.title.split("-")[0];
-    const idx = parseInt(e.target.attrs.title.split("-")[1])
+    const idx = parseInt(e.target.attrs.title.split("-")[1]);
     console.log(idx);
     setShapes((prev) => {
-      console.log({ prev, idx })
-      prev[name][idx].x = e.target.x();
-      prev[name][idx].y = e.target.y();
-      return prev;
+      return {
+        ...prev,
+        [name]: prev[name].map((item, index) => {
+          if (index === idx) {
+            return {
+              x: e.target.x(),
+              y: e.target.y()
+            }
+          }
+          return item;
+        })
+      }
     })
   }
   return (
-    <div className="App" style={{ overflow: "hidden" }}>
-      <div style={{ display: "flex", margin: "20px", fontSize: "2rem" }}>
-        <div style={{ marginRight: "10px" }} id="circle" onClick={(e) => { handleObjects(e) }}>
-          Circle <span>
-            <CircleIcon />
-          </span>
-        </div>
-        <div style={{ marginRight: "10px" }} id="square" onClick={(e) => { handleObjects(e) }}>
-          Square <span>
-            <SquareIcon />
-          </span>
-        </div>
-      </div>
+    <div className="App" style={{ overflow: "hidden", height: "100vh", display: "flex", flexDirection: "column" }}>
+
+      <Stack direction="row" spacing={2} sx={{ margin: "10px" }}>
+        <Button variant="contained" color="success" id="circle" onClick={(e) => { handleObjects(e) }} endIcon={<CircleIcon />}>
+          Circle
+        </Button>
+        <Button variant="contained" color="error" id="square" onClick={(e) => { handleObjects(e) }} endIcon={<SquareIcon />}>
+          Square
+        </Button>
+        <Button variant="outlined" onClick={handleClearCanvas} startIcon={<DeleteIcon />}>
+          Delete
+        </Button>
+      </Stack>
       <Canvas objects={objects} handleObjects={handleObjects} handleDragEnd={handleDragEnd} circles={shapes.circle} squares={shapes.square} />
-    </div >
+    </div>
   );
 }
 
